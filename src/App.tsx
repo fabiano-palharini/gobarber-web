36f20e377a61d9cf543316009118d78b02b5c5e1
Fabiano Palharini <fabianofalco@gmail.com>
2020-05-06 22:55:33 -0300

    global style definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react';
+import GlobalStyle from './styles/global';
 
-const App: FC = () => <h1>Hello !!!</h1>;
+const App: FC = () => (
+  <>
+    <h1>Hello !!!</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
