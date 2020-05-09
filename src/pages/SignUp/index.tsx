d8ee8aecb9a904aaa5e9fc0b0ff79e3a10ce3b5b
Fabiano Palharini <fabianofalco@gmail.com>
2020-05-09 15:41:34 -0300

    adding unform lib

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Sign-Up</h1>
-        <Input name="name" icon={FiUser} placeholder="Name" />
-        <Input name="email" icon={FiMail} placeholder="E-Mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Button type="submit">Sign Up</Button>
-      </form>
-      <a href="/">
-        <FiArrowLeft />
-        Back to logon
-      </a>
-    </Content>
-  </Container>
-);
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        {/* <Form initialData={{ name: 'default value' }} onSubmit={handleSubmit}> */}
+        <Form onSubmit={handleSubmit}>
+          <h1>Sign-Up</h1>
+          <Input name="name" icon={FiUser} placeholder="Name" />
+          <Input name="email" icon={FiMail} placeholder="E-Mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">Sign Up</Button>
+        </Form>
+        <a href="/">
+          <FiArrowLeft />
+          Back to logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
