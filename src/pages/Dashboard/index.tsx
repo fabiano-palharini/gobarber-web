fa69b25ae70fbf9b4dc2c99045c891d20f419545
Fabiano Palharini <fabianofalco@gmail.com>
2020-07-30 08:28:20 -0300

    layout for schedules in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, Calendar, NextAppointment } from './styles';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
@@ -27,6 +27,30 @@ const Dashboard: React.FC = () => {
 
       </HeaderContent>
     </Header>
+
+    <Content>
+      <Schedule>
+        <h1>Schedules</h1>
+        <p>
+          <span>Today</span>
+          <span>Day 06</span>
+          <span>Monday</span>
+        </p>
+        <NextAppointment>
+          <strong>Next Appointment</strong>
+          <div>
+            <img src="https://avatars1.githubusercontent.com/u/32514208?s=460&v=4" alt="Fabiano Falco" />
+            <strong>Fabiano Falco</strong>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+          </div>
+        </NextAppointment>
+      </Schedule>
+
+      <Calendar />
+    </Content>
   </Container>
   );
 };
